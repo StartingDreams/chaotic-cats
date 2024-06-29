aa0cda1b57b1b9057cdf4d474510e71e6f075e25
David Smith <david@startingdreams.com>
2024-06-29 15:05:16 -0400

    updated schema exports

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "~/server/db/schema";
-// import { env } from "~/env";
 
 export const db = drizzle(sql, { schema });
